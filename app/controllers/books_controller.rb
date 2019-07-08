4e8def9dbdbb13bd7d5c8a5dc8de6ef92cff0d37
meihotate <meihotate@gmail.com>
2019-07-08 18:59:17 +0900

    [提出] 課題2_アプリケーションを作成してみよう_基礎編

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,47 @@
+class BooksController < ApplicationController
+  def top
+  end
+
+  def index
+  	@booksnew = Book.new
+  	@books = Book.all
+  end
+
+  def create
+  	booksnew = Book.new(book_params)
+  	if booksnew.save
+  		flash[:notice] = "Book was successfully created."
+  		redirect_to book_path(booksnew)
+  	end
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	if book.update(book_params)
+  		flash[:notice] = "Book was successfully updated."
+  		redirect_to book_path(book)
+  	end
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	if book.destroy
+  		flash[:notice] = "Book was succesfully destroyed."
+  		redirect_to books_path
+  	end
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+
+end
